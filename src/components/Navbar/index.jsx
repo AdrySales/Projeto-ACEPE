0e608cc826b7a3e24d783064933dc537170af8b4
adrySales <yasmim.adrieny@gmail.com>
2025-01-16 01:18:29 -0300

    ajustes no posicionamento do modulo de login/cadastro e ajuste no redirecionamento dos btns de cadastro e login

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -1,22 +1,29 @@
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 import './Navbar.css';
 
 function Navbar() {
+  const location = useLocation(); // Obter a rota atual
+  
   return (
     <nav className="navbar">
       <div className="navbar-logo">
         <Link to="/"><img src={`${process.env.PUBLIC_URL}/logo-acepe.png`} alt="Logo" className="logo" /></Link>
       </div>
-      <div className="navbar-buttons">
-        <Link to="/login">
-          <button className="btn btn-outline">Login</button>
-        </Link>
-        <Link to="/signup">
-          <button className="btn btn-primary">Cadastro</button>
-        </Link>
-      </div>
+      {/* Renderizar botões apenas se não estiver na rota de login */}
+      {location.pathname === '/' && (
+        <div className="navbar-buttons">
+          <Link to="/login?type=login">
+            <button className="btn btn-outline">Login</button>
+          </Link>
+          <Link to="/signup?type=signup">
+            <button className="btn btn-primary">Cadastro</button>
+          </Link>
+        </div>
+      )}
     </nav>
   );
 }
 
 export default Navbar;
+
+
--- src/pages/LoginCadastroPage/index.jsx
+++ src/pages/LoginCadastroPage/index.jsx
@@ -1,85 +1,78 @@
-import React, { useEffect } from 'react';
-import './LoginCadastro.css'; // Supondo que os estilos estejam em style.css
+import React, { useEffect, useState } from 'react';
+import './LoginCadastro.css'; 
 
 const LoginPage = () => {
+  const [formType, setFormType] = useState('login'); // Estado para controlar qual formulário mostrar
+
   useEffect(() => {
-    const btnSignin = document.querySelector("#signin");
-    const btnSignup = document.querySelector("#signup");
-    const body = document.querySelector("body");
-
-    btnSignin.addEventListener("click", () => {
-      body.className = "sign-in-js";
-    });
-
-    btnSignup.addEventListener("click", () => {
-      body.className = "sign-up-js";
-    });
-
-    return () => {
-      btnSignin.removeEventListener("click", () => {
-        body.className = "sign-in-js";
-      });
-      btnSignup.removeEventListener("click", () => {
-        body.className = "sign-up-js";
-      });
-    };
+    const params = new URLSearchParams(window.location.search);
+    const type = params.get('type');
+    setFormType(type === 'signup' ? 'signup' : 'login');
   }, []);
+
+  useEffect(() => {
+    const body = document.querySelector("body");
+    body.className = formType === 'signup' ? 'sign-up-js' : 'sign-in-js';
+  }, [formType]);
 
   return (
     <div className="container">
-      <div className="content first-content">
-        <div className="first-column">
-          <h2 className="title title-primary">Seja bem-vindo!</h2>
-          <p className="description description-primary">Caso já tenha conta cadastrada</p>
-          <p className="description description-primary">Por favor, clique no botão a baixo </p>
-          <button id="signin" className="btn btn-primary">Login</button>
+      {formType === 'login' ? (
+        <div className="content second-content">
+          <div className="first-column">
+            <h2 className="title title-primary">Seja bem-vindo!</h2>
+            <p className="description description-primary">Ainda não tem conta?</p>
+            <p className="description description-primary">Clique no botão abaixo</p>
+            <button onClick={() => setFormType('signup')} className="btn btn-primary">Cadastro</button>
+          </div>
+          <div className="second-column">
+            <h2 className="title title-second">Fazer Login</h2>
+            <form className="form">
+              <label className="label-input">
+                <i className="far fa-envelope icon-modify"></i>
+                <input type="email" placeholder="Email" />
+              </label>
+              <label className="label-input">
+                <i className="fas fa-lock icon-modify"></i>
+                <input type="password" placeholder="Senha" />
+              </label>
+              <a className="password" href="/">Esqueceu a senha?</a>
+              <button className="btn btn-second">Login</button>
+            </form>
+          </div>
         </div>
-        <div className="second-column">
-          <h2 className="title title-second">Crie uma conta</h2>
-          <form className="form">
-            <label className="label-input">
-              <i className="far fa-user icon-modify"></i>
-              <input type="text" placeholder="Name" />
-            </label>
-            <label className="label-input">
-              <i className="far fa-envelope icon-modify"></i>
-              <input type="email" placeholder="Email" />
-            </label>
-            <label className="label-input">
-              <i className="far fa-envelope icon-modify"></i>
-              <input type="email" placeholder="Confirmação de Email" />
-            </label>
-            <label className="label-input">
-              <i className="fas fa-lock icon-modify"></i>
-              <input type="password" placeholder="Senha" />
-            </label>
-            <button className="btn btn-second">Cadastrar</button>
-          </form>
+      ) : (
+        <div className="content first-content">
+          <div className="first-column">
+            <h2 className="title title-primary">Seja bem-vindo!</h2>
+            <p className="description description-primary">Caso já tenha conta cadastrada</p>
+            <p className="description description-primary">Por favor, clique no botão abaixo</p>
+            <button onClick={() => setFormType('login')} className="btn btn-primary">Login</button>
+          </div>
+          <div className="second-column">
+            <h2 className="title title-second">Crie uma conta</h2>
+            <form className="form">
+              <label className="label-input">
+                <i className="far fa-user icon-modify"></i>
+                <input type="text" placeholder="Nome" />
+              </label>
+              <label className="label-input">
+                <i className="far fa-envelope icon-modify"></i>
+                <input type="email" placeholder="Email" />
+              </label>
+              <label className="label-input">
+                <i className="far fa-envelope icon-modify"></i>
+                <input type="email" placeholder="Confirmação de Email" />
+              </label>
+              <label className="label-input">
+                <i className="fas fa-lock icon-modify"></i>
+                <input type="password" placeholder="Senha" />
+              </label>
+              <button className="btn btn-second">Cadastrar</button>
+            </form>
+          </div>
         </div>
-      </div>
-      <div className="content second-content">
-        <div className="first-column">
-          <h2 className="title title-primary">Seja bem-vindo!</h2>
-          <p className="description description-primary">Ainda não tem conta?</p>
-          <p className="description description-primary">Clique no botão a baixo</p>
-          <button id="signup" className="btn btn-primary">Cadastro</button>
-        </div>
-        <div className="second-column">
-          <h2 className="title title-second">Fazer Login</h2>
-          <form className="form">
-            <label className="label-input">
-              <i className="far fa-envelope icon-modify"></i>
-              <input type="email" placeholder="Email" />
-            </label>
-            <label className="label-input">
-              <i className="fas fa-lock icon-modify"></i>
-              <input type="password" placeholder="Senha" />
-            </label>
-            <a className="password" href="/">Esqueceu a senha?</a>
-            <button className="btn btn-second">Login</button>
-          </form>
-        </div>
-      </div>
+      )}
     </div>
   );
 };
